95cf7b14caf3b608baf07977dd68154343d05689
Tushar_Maity <68024552+Tushar-Maity@users.noreply.github.com>
2021-05-17 19:27:25 +0530

    Banner added.

---
+++ src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+    return (
+        <div className="home">
+            <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/Apple/May/MSD/iPhone12/D23407104_IN_WL_MSD_iPhone12_DesktopTallHero_1500x600._CB669806582_.jpg" alt="" />
+        </div>
+    )
+}
+
+export default Home
