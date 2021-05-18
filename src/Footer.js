7d4db6eb1be13fdd6e2058367498b6f6e39ad6fb
Tushar_Maity <68024552+Tushar-Maity@users.noreply.github.com>
2021-05-18 18:38:13 +0530

    Footer Modified

--- src/Footer.js
+++ src/Footer.js
@@ -9,6 +9,156 @@ function Footer() {
                     Back to top
                 </span>
             </div>
+            <div className="navFooterVerticalColumn navAccessibility">
+                <div className="navFooterVerticalRow navAccessibility">
+                    <div className="navFooterLinkCol navAccessibility">
+                        <div className="navFooterColHead">Get to Know Us</div>
+                        <ul>
+                            <li className="nav_first">
+                                <span className="nav_a">About Us</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Careers</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Press Releases</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Amazon Cares</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Gift a Smile</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="navFooterColSpacerInner navAccessibilty"></div>
+                    <div className="navFooterLinkCol navAccessibility">
+                        <div className="navFooterColHead">Connect with Us</div>
+                        <ul>
+                            <li className="nav_first">
+                                <span className="nav_a">Facebook</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Twitter</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Instagram</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="navFooterColSpacerInner navAccessibilty"></div>
+                    <div className="navFooterLinkCol navAccessibility">
+                        <div className="navFooterColHead">Make Money with Us</div>
+                        <ul>
+                            <li className="nav_first">
+                                <span className="nav_a">Sell on Amazon</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Sell under Amazon Accelerator</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Amazon Global Selling</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Become a Affiliate</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Fulfilment by Amazon</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Advertise Your Products</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Amazon Pay on Merchants</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="navFooterColSpacerInner navAccessibilty"></div>
+                    <div className="navFooterLinkCol navAccessibility">
+                        <div className="navFooterColHead">Let Us Help You</div>
+                        <ul>
+                            <li className="nav_first">
+                                <span className="nav_a">COVID-19 and Amazon</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Your Account</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Returns Centre</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">100% Purchase Protection</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Amazon App Download</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Amazon Assistant Download</span>
+                            </li>
+                            <li className="nav_first">
+                                <span className="nav_a">Help</span>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="navFooterColSpacerInner navAccessibilty"></div>
+                </div>
+            </div>
+            <div className="nav-footer-line"></div>
+            <div className="navFooterLine navFooterLinkLine navFooterPadItemLine">
+                <ul>
+                    <li className="nav_first">
+                        <span className="nav_a">Australia</span>
+                    </li>
+                    <li>
+                        <span className="nav_a">Brazil</span>
+                    </li>
+                    <li>
+                        <span className="nav_a">Canada</span>
+                    </li>
+                    <li>
+                        <span className="nav_a">China</span>
+                    </li>
+                    <li>
+                        <span className="nav_a">France</span>
+                    </li>
+                    <li>
+                        <span className="nav_a">Germany</span>
+                    </li>
+                    <li>
+                        <span className="nav_a">Italy</span>
+                    </li>
+                    <li>
+                        <span className="nav_a">Japan</span>
+                    </li>
+                    <li>
+                        <span className="nav_a">Mexico</span>
+                    </li>
+                    <li>
+                        <span className="nav_a">Netherlands</span>
+                    </li>
+                    <li>
+                        <span className="nav_a">Poland</span>
+                    </li>
+                    <li>
+                        <span className="nav_a">Singapore</span>
+                    </li>
+                    <li>
+                        <span className="nav_a">Spain</span>
+                    </li>
+                    <li>
+                        <span className="nav_a">Turkey</span>
+                    </li>
+                    <li>
+                        <span className="nav_a">United Arab Emirates</span>
+                    </li>
+                    <li>
+                        <span className="nav_a">United Kingdom</span>
+                    </li>
+                    <li className="nav_last">
+                        <span className="nav_a">United States</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
